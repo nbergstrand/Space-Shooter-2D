d4ffe7d60554e3f5c4983781ae6b0f0c5e38c88a
nbergstrand <bergstrand.niklas@gmail.com>
2021-03-19 07:51:30 +0000

    Add Material and inital player script

---
+++ Assets/Space Shooter 2D/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+    [SerializeField]
+    float speed = 3.5f;
+        
+    void Start()
+    {
+        //Reset the player position when game starts
+        transform.position = new Vector3(0f, 1f, 0f);
+
+
+
+    }
+
+    void Update()
+    {
+        transform.Translate( Vector3.right * speed *  Time.deltaTime);
+    }
+}
