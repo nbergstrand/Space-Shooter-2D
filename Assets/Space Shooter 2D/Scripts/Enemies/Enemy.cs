7ab3b216974e405bd0fd2161a060f0cc817b6156
nbergstrand <bergstrand.niklas@gmail.com>
2021-05-01 09:50:57 +0100

    Enemy Avoid Shot

--- Assets/Space Shooter 2D/Scripts/Enemies/Enemy.cs
+++ Assets/Space Shooter 2D/Scripts/Enemies/Enemy.cs
@@ -18,7 +18,7 @@ public class Enemy : MonoBehaviour
 
     [SerializeField]
     Vector3 _projectileSpawnPos;
-        
+
     float _timeToNextShot;
 
     bool _isDead = false;
@@ -31,7 +31,7 @@ public class Enemy : MonoBehaviour
     GameManager _gameManager;
     Animator _animator;
     AudioManager _audioManager;
-       
+
 
     /*****************Phase 2***************/
 
@@ -62,6 +62,8 @@ public class Enemy : MonoBehaviour
 
     float _timeToNextShotOnPowerUp = 0;
 
+    float dodgeDistance = 3;
+
 
     /**************************************************/
 
@@ -71,7 +73,7 @@ public class Enemy : MonoBehaviour
         /*****************New Enemy Movement***************/
         int randomStartDirection = Random.Range(0, 2);
 
-        if(randomStartDirection == 0)
+        if (randomStartDirection == 0)
         {
             _moveLeft = false;
             _moveRight = true;
@@ -92,15 +94,15 @@ public class Enemy : MonoBehaviour
         /*********************Enemy Shields***********/
         spawnManager = GameObject.Find("Game_Manager").GetComponent<SpawnManager>();
 
-        if(spawnManager.CurrentWave > 3)
+        if (spawnManager.CurrentWave > 3)
         {
             int randomChance = Random.Range(1, 3);
 
-            if(randomChance == 1)
+            if (randomChance == 1)
             {
                 EnableShield();
             }
-            
+
         }
 
         /**************************************************/
@@ -112,7 +114,15 @@ public class Enemy : MonoBehaviour
 
         /**************************************************/
 
-        if( _enemyType == EnemyType.SmartShooter)
+        /*********************Dodger Enemy Type***********/
+
+        if (_enemyType == EnemyType.Dodger)
+            InvokeRepeating("LookForProjectiles", 0f, 0.15f);
+
+        /**************************************************/
+
+
+        if (_enemyType == EnemyType.SmartShooter)
         {
             _timeToNextShot = 0f;
         }
@@ -132,7 +142,7 @@ public class Enemy : MonoBehaviour
 
 
         _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
-        
+
 
         if (_gameManager == null)
         {
@@ -152,23 +162,23 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        if(!_isDead)
+        if (!_isDead)
         {
-            if(_enemyType == EnemyType.StraightShooter || _enemyType == EnemyType.Burster)
+            if (_enemyType == EnemyType.StraightShooter || _enemyType == EnemyType.Burster)
                 Shoot(false);
 
-            if(_enemyType == EnemyType.SmartShooter)
+            if (_enemyType == EnemyType.SmartShooter)
             {
                 RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position + Vector2.up, Vector2.up);
 
-                
+
                 if (hit.collider != null)
                 {
-                   
+
                     if (hit.collider.tag == "Player")
                         Shoot(false);
                 }
-                             
+
             }
             /***************************Enemy Pickups****************************/
             if (_enemyType == EnemyType.StraightShooter || _enemyType == EnemyType.SideToSide)
@@ -192,7 +202,7 @@ public class Enemy : MonoBehaviour
     void EnemyMovement()
     {
 
-        switch(_enemyType)
+        switch (_enemyType)
         {
             case EnemyType.SideToSide:
                 if (transform.position.x < -9f)
@@ -220,48 +230,48 @@ public class Enemy : MonoBehaviour
                 break;
 
             case EnemyType.Burster:
-                transform.Translate(Vector3.down  * _speed * Time.deltaTime);
+                transform.Translate(Vector3.down * _speed * Time.deltaTime);
                 transform.localPosition = new Vector3(startPosX + WaveMovement(_waveMovementSpeed, _waveMovementAmount), transform.localPosition.y, transform.localPosition.z);
                 break;
 
-                /***************************************Aggressive Enemy Type***************************************/
+            /***************************************Aggressive Enemy Type***************************************/
 
-                case EnemyType.Kamikaze:
-                    if(_target == null)
+            case EnemyType.Kamikaze:
+                if (_target == null)
+                {
+                    if (transform.position.x < -9f)
                     {
-                        if (transform.position.x < -9f)
-                        {
-                             
-                            _moveLeft = false;
-                            _moveRight = true;
-                        }
 
-                        if (transform.position.x > 11f)
-                        {
-                            
-                            _moveLeft = true;
-                            _moveRight = false;
-                        }
-
-                        if (_moveRight)
-                            transform.Translate((Vector3.up + Vector3.left) * _speed * Time.deltaTime);
-
-                        if (_moveLeft)
-                            transform.Translate((Vector3.up + Vector3.right) * _speed * Time.deltaTime);
+                        _moveLeft = false;
+                        _moveRight = true;
                     }
-                    else
+
+                    if (transform.position.x > 11f)
                     {
-                        float rotationSpeed = 100;
 
-                        //Rotate towards the player smoothly
-                        Quaternion rotatation = Quaternion.FromToRotation(Vector3.up, _target.position - transform.position);
-                        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotatation, rotationSpeed * Time.deltaTime);
-
-
-                        //Move towards target. Relative space needs to be set to self or the object will just move upwards or downwards even if turned towards target
-                        transform.Translate(Vector3.up * _speed * Time.deltaTime, Space.Self);
+                        _moveLeft = true;
+                        _moveRight = false;
                     }
-                    break;
+
+                    if (_moveRight)
+                        transform.Translate((Vector3.up + Vector3.left) * _speed * Time.deltaTime);
+
+                    if (_moveLeft)
+                        transform.Translate((Vector3.up + Vector3.right) * _speed * Time.deltaTime);
+                }
+                else
+                {
+                    float rotationSpeed = 100;
+
+                    //Rotate towards the player smoothly
+                    Quaternion rotatation = Quaternion.FromToRotation(Vector3.up, _target.position - transform.position);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, rotatation, rotationSpeed * Time.deltaTime);
+
+
+                    //Move towards target. Relative space needs to be set to self or the object will just move upwards or downwards even if turned towards target
+                    transform.Translate(Vector3.up * _speed * Time.deltaTime, Space.Self);
+                }
+                break;
 
             /*******************************************************************************************************/
             /***************************************Smart Enemy Type***************************************/
@@ -280,64 +290,86 @@ public class Enemy : MonoBehaviour
                 }
 
                 if (_moveRight)
-                    transform.Translate( Vector3.right * _speed * Time.deltaTime);
+                    transform.Translate(Vector3.right * _speed * Time.deltaTime);
 
                 if (_moveLeft)
-                    transform.Translate( Vector3.left * _speed * Time.deltaTime);
+                    transform.Translate(Vector3.left * _speed * Time.deltaTime);
 
                 break;
-                /*******************************************************************************************************/
-        }
+            /*******************************************************************************************************/
 
 
+            case EnemyType.Dodger:
 
-                if (transform.position.y < -8f && !_isDead)
-                    Destroy(gameObject);
-        }
+                transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
-        private void Shoot(bool randomCoolDown)
-        {
+                float distanceToProjectile = 5;
 
-            if (Time.time > _timeToNextShot)
-            {
-               if(randomCoolDown == false)
+                if (_target != null)
                 {
-                    _timeToNextShot = Time.time + 1f;
+                    distanceToProjectile = Vector2.Distance(transform.position, _target.transform.position);
+
                 }
-               else
+
+                if (distanceToProjectile < dodgeDistance && _target.position.y < transform.position.y)
                 {
-                    _timeToNextShot = Time.time + Random.Range(3, 6);
+                    transform.position = Vector2.MoveTowards(transform.position, _target.position, -8 * Time.deltaTime);
+                    
                 }
                     
 
-                GameObject laser = Instantiate(_projectile, transform.position + _projectileSpawnPos, Quaternion.identity);
-
-                laser.transform.parent = _projectileParent;
-
-
-                _audioManager.PlayLaserSound();
-
-            }
+                break;
         }
-        /***************************Enemy Pickups****************************/
-        private void ShootPowerUp()
+
+
+
+        if (transform.position.y < -8f && !_isDead)
+            Destroy(gameObject);
+    }
+
+    private void Shoot(bool randomCoolDown)
+    {
+
+        if (Time.time > _timeToNextShot)
+        {
+            if (randomCoolDown == false)
+            {
+                _timeToNextShot = Time.time + 1f;
+            }
+            else
+            {
+                _timeToNextShot = Time.time + Random.Range(3, 6);
+            }
+
+
+            GameObject laser = Instantiate(_projectile, transform.position + _projectileSpawnPos, Quaternion.identity);
+
+            laser.transform.parent = _projectileParent;
+
+
+            _audioManager.PlayLaserSound();
+
+        }
+    }
+    /***************************Enemy Pickups****************************/
+    private void ShootPowerUp()
+    {
+
+        if (Time.time > _timeToNextShotOnPowerUp)
         {
 
-            if (Time.time > _timeToNextShotOnPowerUp)
-            {
+            _timeToNextShotOnPowerUp = Time.time + 1f;
 
-                _timeToNextShotOnPowerUp = Time.time + 1f;
-            
-                GameObject laser = Instantiate(_projectile, transform.position + _projectileSpawnPos, Quaternion.identity);
+            GameObject laser = Instantiate(_projectile, transform.position + _projectileSpawnPos, Quaternion.identity);
 
-                laser.transform.parent = _projectileParent;
-            
-                _audioManager.PlayLaserSound();
+            laser.transform.parent = _projectileParent;
 
-            }
+            _audioManager.PlayLaserSound();
 
         }
-        /*********************************************************************/
+
+    }
+    /*********************************************************************/
 
 
 
@@ -349,9 +381,9 @@ public class Enemy : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
-            
+
             if (other.GetComponent<Player>() != null)
             {
                 other.GetComponent<Player>().DamagePlayer();
@@ -410,14 +442,14 @@ public class Enemy : MonoBehaviour
                 GetComponent<Renderer>().enabled = false;
                 GameObject explosionGO = Instantiate(explosion, transform.position, Quaternion.identity);
                 Destroy(explosionGO, 2f);
-                
+
 
             }
             _audioManager.PlayExplosionSound();
             _isDead = true;
             _speed = 0;
             GetComponent<BoxCollider2D>().enabled = false;
-            
+
             Destroy(gameObject, 3f);
         }
     }
@@ -443,22 +475,54 @@ public class Enemy : MonoBehaviour
 
         GameObject player = GameObject.FindGameObjectWithTag("Player");
 
-        float chaseDistance  = 4;
-        
-        
+        float chaseDistance = 4;
+
+
         float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-                
+
         if (distanceToPlayer < chaseDistance)
         {
             _target = player.transform;
             _gotTarget = true;
 
         }
-        
+
     }
     /*******************************************************************************************************/
+    private void LookForProjectiles()
+    {
 
+        GameObject[] projectiles = GameObject.FindGameObjectsWithTag("Projectile");
 
+        float shortestDistance = Mathf.Infinity;
+
+        GameObject nearestProjectile = null;
+
+        foreach (GameObject projectile in projectiles)
+        {
+
+            float distanceToProjectile = Vector2.Distance(transform.position, projectile.transform.position);
+
+            if (distanceToProjectile < shortestDistance)
+            {
+                shortestDistance = distanceToProjectile;
+
+                nearestProjectile = projectile;
+            }
+        }
+
+        //If an enemy has been found set target
+        if (nearestProjectile != null)
+        {
+            _target = nearestProjectile.transform;
+            _gotTarget = true;
+        }
+        else
+        {
+            _target = null;
+        }
+
+    }
 }
 
 
@@ -468,6 +532,7 @@ public enum EnemyType
     StraightShooter,
     Burster,
     Kamikaze,
-    SmartShooter
+    SmartShooter,
+    Dodger
 
 }
